08706138058f2fda6ec45d868c1aabde540afa96
Yoshiki Wada <yoshiki.wada@gmail.com>
2015-01-11 09:49:54 +0900

    simplify orddiff.ml

--- a.ml
+++ a.ml
@@ -9,7 +9,7 @@ let l1 = 1.0
 let l2 = 1.0
 let g = 9.8
 
-let x_ary = [|
+let xs = [|
   0.99 *. pi;  (* phi *)
   0.5 *. pi;  (* theta *)
   0.0;  (* mu = d_phi *)
@@ -21,69 +21,35 @@ let n_theta = 1
 let n_mu    = 2
 let n_nu    = 3
 
-let xdot_ary =
+let dxdt _t xs fs =
   let open Flop in
+
+  let phi = xs.(n_phi) in
+  let theta = xs.(n_theta) in
+  let mu = xs.(n_mu) in
+  let nu = xs.(n_nu) in
+
   let c_a = (m1+m2)*l1**2.0 in
+  let c_b = m2*l1*l2*cos(phi-theta) in
+  let c_c = m2*l1*l2*sin(phi-theta) in
+  let c_d = (m1+m2)*g*l1*sin(phi) in
   let c_e = m2*l2 in
-  [|
-    (* phi *)
-    (fun ~t ~x_ary ->
-       ignore t;
-       x_ary.(n_mu));
+  let c_f = m2*l1*l2*cos(phi-theta) in
+  let c_g = -m2*l1*l2*sin(phi-theta) in
+  let c_h = m2*g*l2*sin(theta) in
 
-    (* theta *)
-    (fun ~t ~x_ary ->
-       ignore t;
-       x_ary.(n_nu));
+  fs.(n_phi) <- xs.(n_mu);
+  fs.(n_theta) <- xs.(n_nu);
+  fs.(n_mu) <- -1.0/(c_a*c_e-c_f*c_b) * (c_c*c_e*nu**2.0 - c_g*c_b*mu**2.0 + c_d*c_e - c_h*c_b);
+  fs.(n_nu) <- -1.0/(c_b*c_f-c_e*c_a) * (c_c*c_f*nu**2.0 - c_g*c_a*mu**2.0 + c_d*c_f - c_h*c_a)
 
-    (* mu *)
-    (fun ~t ~x_ary ->
-       ignore t;
-       let phi = x_ary.(n_phi) in
-       let theta = x_ary.(n_theta) in
-       let mu = x_ary.(n_mu) in
-       let nu = x_ary.(n_nu) in
-
-       let c_b = m2*l1*l2*cos(phi-theta) in
-       let c_c = m2*l1*l2*sin(phi-theta) in
-       let c_d = (m1+m2)*g*l1*sin(phi) in
-       let c_f = m2*l1*l2*cos(phi-theta) in
-       let c_g = -m2*l1*l2*sin(phi-theta) in
-       let c_h = m2*g*l2*sin(theta) in
-       -1.0/(c_a*c_e-c_f*c_b) * (c_c*c_e*nu**2.0 - c_g*c_b*mu**2.0 + c_d*c_e - c_h*c_b)
-    );
-
-    (* nu *)
-    (fun ~t ~x_ary ->
-       ignore t;
-       let phi = x_ary.(n_phi) in
-       let theta = x_ary.(n_theta) in
-       let mu = x_ary.(n_mu) in
-       let nu = x_ary.(n_nu) in
-
-       let c_b = m2*l1*l2*cos(phi-theta) in
-       let c_c = m2*l1*l2*sin(phi-theta) in
-       let c_d = (m1+m2)*g*l1*sin(phi) in
-       let c_f = m2*l1*l2*cos(phi-theta) in
-       let c_g = -m2*l1*l2*sin(phi-theta) in
-       let c_h = m2*g*l2*sin(theta) in
-       -1.0/(c_b*c_f-c_e*c_a) * (c_c*c_f*nu**2.0 - c_g*c_a*mu**2.0 + c_d*c_f - c_h*c_a)
-    );
-|]
-
-let loop n dt ode =
+let loop n ode xs =
   let rec aux i =
     if i > n then
       ()
     else begin
-      let t = dt *. float i in
-      let phi = (ode.Orddiff.Base.get_x_ary ()).(n_phi) in
-      let theta = (ode.Orddiff.Base.get_x_ary ()).(n_theta) in
-  (*
-      let mu = (ode.Orddiff.Base.get_x_ary ()).(n_mu) in
-      let nu = (ode.Orddiff.Base.get_x_ary ()).(n_nu) in
-   *)
-      printf "%g\t%g\t%g\n" t phi theta;
+      let t = ode.Orddiff.Base.get_t () in
+      printf "%g\t%g\t%g\n" t xs.(n_phi) xs.(n_theta);
       ode.Orddiff.Base.update ();
       aux (i + 1)
     end
@@ -93,5 +59,5 @@ let loop n dt ode =
 let () =
   let n = 1000000 in
   let dt = 0.001 in
-  let ode = Orddiff.Rk4.init ~dt ~x_ary ~xdot_ary in
-  loop n dt ode
+  let ode = Orddiff.Rk4.init ~dt ~dxdt ~xs in
+  loop n ode xs
